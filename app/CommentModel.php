f0250e3d722f27e6278df912aa1059bbf84d7cb7
3A532089 <likebestupid@gmail.com>
2018-12-31 21:18:50 +0800

    commentModel增加關聯

--- app/CommentModel.php
+++ app/CommentModel.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Post;
+
 class CommentModel extends Model
 {
     protected $table='comments';
+
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
 }
