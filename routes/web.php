c1ed90ec6efa7fc702a743e130f3b6f5ae1fa0b0
3A532089 <likebestupid@gmail.com>
2018-12-31 20:47:53 +0800

    新增路由

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    \App\Post::create([
+        'title'=>'testtitle',
+        'content'=>'testcontent',
+        ]);
 });
