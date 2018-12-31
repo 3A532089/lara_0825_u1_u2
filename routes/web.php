a123117b2060b687b5ee56872634a3be55d66759
3A532089 <likebestupid@gmail.com>
2018-12-31 21:03:10 +0800

    取得collection

--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,10 @@ Route::get('/', function () {
     /*$post = \App\Post::find(1);
     $post->delete();*/
 
-    \App\Post::destroy(3, 5, 7);
+    //\App\Post::destroy(3, 5, 7);
+
+    $allPosts = \App\Post::all();
+    dd($allPosts);
+    $featuredPosts = \App\Post::where('is_feature', 1)‐>get();
+    dd($featuredPosts);
 });
